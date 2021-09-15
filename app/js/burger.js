19d053f70340c876c31748463a0439cc2e71fb14
Dispersioned <dispersioned@gmail.com>
2021-09-15 07:22:37 +0300

    add: header p3: burger btn, partial nav menu

--- app/js/burger.js
+++ app/js/burger.js
@@ -1,13 +1,16 @@
 const burgerInit = function () {
-	let burgerIcon = document.querySelector('.header__burger');
+	let burgerTrigger = document.querySelector('.header__burger');
+	let burgerIcon = document.querySelector('.header__burger-icon');
+	let headerBurgerTitle = document.querySelector('.header__burger-title');
 	let burgerBody = document.querySelector('.header__nav');
 
-	if (burgerIcon) {
-		burgerIcon.addEventListener('click', function (e) {
+	if (burgerTrigger) {
+		burgerTrigger.addEventListener('click', function (e) {
 			document.querySelector('body').classList.toggle('_lock');
 
 			burgerIcon.classList.toggle('_active');
 			burgerBody.classList.toggle('_active');
+			headerBurgerTitle.classList.toggle('_active');
 		});
 	}
 }
