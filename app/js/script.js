4d02ebf1ccae65236fd364b6e04109f4db9f1b5f
Dispersioned <dispersioned@gmail.com>
2021-11-02 16:37:54 +0300

    faq section finished

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,7 @@
 @@include('ibg.js');
 @@include('modifiers.js');
 @@include('burger.js');
-// @@ 
-// include('spoiler.js');
+@@ include('spoiler.js');
 // @@ 
 // include('popup.js');
 @@include('general.js');
